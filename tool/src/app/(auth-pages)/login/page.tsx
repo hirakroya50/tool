7e32b6e1d7a57cacfeccec02f1e58790e14ac48b
hirakm0ventures <hirakroya50@gmail.com>
2024-11-23 21:54:40 +0530

    add dark theme by tailwind css

--- tool/src/app/(auth-pages)/login/page.tsx
+++ tool/src/app/(auth-pages)/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import axios from "axios";
+import { useState } from "react";
 import { useForm, type SubmitHandler } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 import { useLoginHook } from "~/hooks/auth";
@@ -46,6 +47,7 @@ export default function LoginPage() {
           withCredentials: true,
         },
       );
+      console.log("logout==", res);
       // Redirect or clear client-side state
     } catch (error) {
       console.error("Error during logout:", error);
@@ -58,117 +60,122 @@ export default function LoginPage() {
     data: dataApiRequestWithHttpCookie,
   } = useAccessTokenTest();
   console.log(dataApiRequestWithHttpCookie, "=====apiRequestWithHttpCookie");
+  const [isDarkMode, setIsDarkMode] = useState(false);
   return (
-    <div className="flex h-screen items-center justify-center bg-gray-900">
-      <div className="w-full max-w-md rounded-lg bg-gray-800 p-8 shadow-lg">
-        <h2 className="mb-6 text-center text-2xl font-bold text-gray-200">
-          Login
-        </h2>
-        <p className="font-semibold text-gray-300 underline">
-          Painting work I can do:
-        </p>
-        <p className="text-gray-300">
-          1. Make the UI look like dark mode by tailwind config
-        </p>
-        <p className="text-gray-300">
-          2. The `apiRequestWithHttpCookie` API call ist working, possibly due
-          to login integration issues with tRPC or another problem.
-        </p>
-        <p className="text-gray-300">
-          3. Use a custom hook for the API call or tRPC for API handling.
-        </p>
+    <div className={isDarkMode ? "dark" : ""}>
+      <button
+        className="m-4 rounded bg-blue-500 p-2 text-white"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        Toggle Dark Mode
+      </button>
+      <div className="dark:bg-dark-background flex h-screen items-center justify-center bg-gray-100">
+        <div className="dark:bg-dark-card w-full max-w-md rounded-lg bg-white p-8 shadow-lg">
+          <h2 className="dark:text-dark-textPrimary mb-6 text-center text-2xl font-bold text-gray-700">
+            Login
+          </h2>
+          <p className="dark:text-dark-textSecondary font-semibold text-gray-500 underline">
+            Painting work I can do:
+          </p>
+          <p className="dark:text-dark-textSecondary text-gray-600">
+            1. Make the UI look like dark mode
+          </p>
+          <p className="dark:text-dark-textSecondary text-gray-600">
+            2. The `apiRequestWithHttpCookie` API call isnt working, possibly
+            due to login integration issues with tRPC or another problem.
+          </p>
+          <p className="dark:text-dark-textSecondary text-gray-600">
+            3. Use a custom hook for the API call or tRPC for API handling.
+          </p>
 
-        {/* <button className="border bg-blue-400" onClick={reset}>
-        Reset
-      </button> */}
+          <form onSubmit={handleSubmit(onSubmit)} noValidate>
+            <div className="mb-4">
+              <label
+                htmlFor="email"
+                className="dark:text-dark-textPrimary mb-2 block text-sm font-medium text-gray-700"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                {...register("email", {
+                  required: "Email is required",
+                  pattern: {
+                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                    message: "Please enter a valid email address",
+                  },
+                })}
+                className={`dark:bg-dark-card dark:text-dark-textPrimary w-full rounded-lg border bg-gray-100 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 ${
+                  errors.email
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-blue-500"
+                }`}
+              />
+              {errors.email && (
+                <p className="mt-2 text-sm text-red-500">
+                  {errors.email.message}
+                </p>
+              )}
+            </div>
 
-        <form onSubmit={handleSubmit(onSubmit)} noValidate>
-          <div className="mb-4">
-            <label
-              htmlFor="email"
-              className="mb-2 block text-sm font-medium text-gray-300"
+            <div className="mb-4">
+              <label
+                htmlFor="password"
+                className="dark:text-dark-textPrimary mb-2 block text-sm font-medium text-gray-700"
+              >
+                Password
+              </label>
+              <input
+                id="password"
+                type="password"
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {
+                    value: 6,
+                    message: "Password must be at least 6 characters long",
+                  },
+                })}
+                className={`dark:bg-dark-card dark:text-dark-textPrimary w-full rounded-lg border bg-gray-100 px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 ${
+                  errors.password
+                    ? "border-red-500 focus:ring-red-500"
+                    : "focus:ring-blue-500"
+                }`}
+              />
+              {errors.password && (
+                <p className="mt-2 text-sm text-red-500">
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              disabled={isLoading}
+              className="dark:bg-dark-card dark:hover:bg-dark-border w-full rounded-lg bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              {...register("email", {
-                required: "Email is required",
-                pattern: {
-                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                  message: "Please enter a valid email address",
-                },
-              })}
-              className={`w-full rounded-lg border bg-gray-700 px-4 py-2 text-gray-300 focus:outline-none focus:ring-2 ${
-                errors.email
-                  ? "border-red-500 focus:ring-red-500"
-                  : "focus:ring-blue-500"
-              }`}
-            />
-            {errors.email && (
-              <p className="mt-2 text-sm text-red-500">
-                {errors.email.message}
-              </p>
-            )}
-          </div>
-
-          <div className="mb-4">
-            <label
-              htmlFor="password"
-              className="mb-2 block text-sm font-medium text-gray-300"
+              {isLoading ? "Loading..." : "Login"}
+            </button>
+          </form>
+          <div className="dark:bg-dark-card mt-4 flex flex-col justify-center rounded-md border bg-gray-700">
+            <button
+              className="dark:bg-dark-card dark:text-dark-textPrimary m-4 rounded-lg bg-yellow-600 p-2 text-white hover:bg-yellow-700 dark:hover:bg-yellow-800"
+              onClick={async () => {
+                await apiRequestWithHttpCookie();
+              }}
+              disabled={buttonLoadingApiRequestWithHttpCookie} // Disable button while loading
             >
-              Password
-            </label>
-            <input
-              id="password"
-              type="password"
-              {...register("password", {
-                required: "Password is required",
-                minLength: {
-                  value: 6,
-                  message: "Password must be at least 6 characters long",
-                },
-              })}
-              className={`w-full rounded-lg border bg-gray-700 px-4 py-2 text-gray-300 focus:outline-none focus:ring-2 ${
-                errors.password
-                  ? "border-red-500 focus:ring-red-500"
-                  : "focus:ring-blue-500"
-              }`}
-            />
-            {errors.password && (
-              <p className="mt-2 text-sm text-red-500">
-                {errors.password.message}
-              </p>
-            )}
+              {buttonLoadingApiRequestWithHttpCookie
+                ? "Loading..."
+                : "Test the access token after login"}
+            </button>
+            <button
+              onClick={handleLogout}
+              className="dark:bg-dark-card dark:text-dark-textPrimary m-4 rounded-lg bg-green-600 p-2 text-white hover:bg-green-700 dark:hover:bg-green-800"
+            >
+              Logout
+            </button>
           </div>
-
-          <button
-            type="submit"
-            disabled={isLoading}
-            className="w-full rounded-lg bg-blue-600 px-4 py-2 text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-          >
-            {isLoading ? "Loading..." : "Login"}
-          </button>
-        </form>
-        <div className="mt-4 flex flex-col justify-center rounded-md border bg-gray-700">
-          <button
-            className="m-4 rounded-lg bg-yellow-600 p-2 text-white hover:bg-yellow-700"
-            onClick={async () => {
-              await apiRequestWithHttpCookie();
-            }}
-            disabled={buttonLoadingApiRequestWithHttpCookie} // Disable button while loading
-          >
-            {buttonLoadingApiRequestWithHttpCookie
-              ? "Loading..."
-              : "Test the access token after login"}
-          </button>
-          <button
-            onClick={handleLogout}
-            className="m-4 rounded-lg bg-green-600 p-2 text-white hover:bg-green-700"
-          >
-            Logout
-          </button>
         </div>
       </div>
     </div>
--- tool/tailwind.config.ts
+++ tool/tailwind.config.ts
@@ -3,10 +3,21 @@ import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+      },
+      colors: {
+        // Define custom dark mode colors
+        dark: {
+          background: "#121212", // Background for dark mode
+          card: "#1E1E1E", // Card background for dark mode
+          textPrimary: "#EDEDED", // Primary text color
+          textSecondary: "#AAAAAA", // Secondary text color
+          border: "#333333", // Border color
+        },
       },
     },
   },
