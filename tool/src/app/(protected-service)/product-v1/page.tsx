ed8fab226016eec2abd5734e8733415bec387a7f
hirakroya50 <hirakroya50@gmail.com>
2024-12-02 20:48:13 +0530

    Revert "set next js api and route , and ingt in fntEnd"
    This reverts commit 52f95582f122a2dacbbf55110d230d7fa010d34f.

--- tool/src/app/(protected-service)/product-v1/page.tsx
+++ tool/src/app/(protected-service)/product-v1/page.tsx
@@ -1,12 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import TrpcTest from "../_components/trpcTest";
-import FirstComponent from "../_components/FirstComponent";
 
 const page = () => {
   return (
     <div>
       <TrpcTest />
-      <FirstComponent />
     </div>
   );
 };
