4a59c7f62fc8830eb700373ed6b108a92b9bc488
hirakroya50 <hirakroya50@gmail.com>
2025-01-08 20:12:50 +0530

    add doc

--- tool/src/app/page.tsx
+++ tool/src/app/page.tsx
@@ -6,15 +6,53 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <Link href={"/login"}>login</Link>
-        <Link href={"/login-trpc"}>login-trpc</Link>
-        <Link href={"/signup-graphql"}>signup-graphql</Link>
-        <Link href={"/s3"}>s3</Link>
-        <Link href={"/debouncing-throttling"}>debouncing-throttling</Link>
-        <Link href={"/notification-service"}>notification-service</Link>
-        <Link href={"/websocket"}>websocket</Link>
-        <Link href={"/websocket2"}>websocket2</Link>
+      <main className="flex min-h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <section className="mb-8 rounded-lg bg-[#1e1e2f] bg-opacity-80 p-6 text-center shadow-xl">
+          <h1 className="bg-cdivp-text mb-6 text-4xl font-extrabold leading-tight text-white underline">
+            Welcome to My Personal Project
+          </h1>
+          <p className="mb-4 font-medium leading-relaxed">
+            This is a full-stack application with a variety of integrated
+            features like authentication, rate liming, file storage, and
+            real-time communication. The backend is built using NestJS
+            microservices, while the frontend is built with Next.js.
+          </p>
+          <p className="mb-4 font-medium leading-relaxed">
+            Explore and test the various functionalities like login, signup,
+            GraphQL integration, WebSocket communication, and more. You can also
+            interact with AWS S3 for file CRUD operations and experiment with
+            features like debouncing, throttling, and notifications.
+          </p>
+        </section>
+
+        <section className="space-y-8 px-6 text-center">
+          <h2 className="text-gradient mb-6 text-3xl font-semibold">
+            Features to Explore:
+          </h2>
+          <div className="flex flex-wrap gap-1 text-lg">
+            {[
+              { href: "/login", label: "Login" },
+              { href: "/login-trpc", label: "Login with TRPC" },
+              { href: "/signup-graphql", label: "Signup with GraphQL" },
+              { href: "/s3", label: "AWS S3 CRUD Operations" },
+              {
+                href: "/debouncing-throttdivng",
+                label: "Debouncing and Throttdivng",
+              },
+              { href: "/notification-service", label: "Notification Service" },
+              { href: "/websocket", label: "WebSocket Chat" },
+              { href: "/websocket2", label: "WebSocket Chat (v2)" },
+            ].map((item, index) => (
+              <Link
+                href={item.href}
+                className="m-0 rounded-lg border p-2 transition-colors duration-300 hover:bg-[#2d2f4d] hover:text-pink-500"
+                key={index}
+              >
+                {item.label}
+              </Link>
+            ))}
+          </div>
+        </section>
       </main>
     </HydrateClient>
   );
