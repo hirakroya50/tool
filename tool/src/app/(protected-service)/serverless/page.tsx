cad3c363634ada2ea7a9c14b2b9c86bee9308436
hirakroya50 <hirakroya50@gmail.com>
2025-01-10 01:30:15 +0530

    remove js to hook

--- tool/src/app/(protected-service)/serverless/page.tsx
+++ tool/src/app/(protected-service)/serverless/page.tsx
@@ -1,90 +1,19 @@
 "use client";
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-
-type Post = {
-  id: number;
-  name: string;
-  updatedAt: string;
-};
-
-interface ApiResponse {
-  data: Post[];
-}
+import React, { useEffect } from "react";
+import { usePosts } from "~/hooks/serverless/usePost";
 
 const PostManager: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<string | null>(null);
-  const [newPostName, setNewPostName] = useState<string>("");
-
-  const API_URL = process.env.NEXT_PUBLIC_SERVERLESS_BACKEND;
-
-  // Fetch all posts
-  const fetchPosts = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await axios.get<ApiResponse>(`${API_URL}`);
-      console.log(response.data.data);
-      setPosts(response?.data?.data);
-    } catch (err) {
-      setError("Failed to fetch posts.");
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Create a new post
-  const createPost = async () => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await axios.post(`${API_URL}`, {
-        postname: newPostName,
-      });
-      setPosts((prev) => [...prev, response.data.post]);
-      setNewPostName("");
-    } catch (err) {
-      setError("Failed to create post.");
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Update a post by ID
-  const updatePost = async (id: number, updatedName: string) => {
-    setLoading(true);
-    setError(null);
-    try {
-      const response = await axios.put(`${API_URL}?id=${id}`, {
-        postname: updatedName,
-      });
-      setPosts((prev) =>
-        prev.map((post) => (post.id === id ? response.data.data : post)),
-      );
-    } catch (err) {
-      setError("Failed to update post.");
-      console.log(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  // Delete a post by ID
-  const deletePost = async (id: number) => {
-    setLoading(true);
-    setError(null);
-    try {
-      await axios.delete(`${API_URL}?id=${id}`);
-      setPosts((prev) => prev.filter((post) => post.id !== id));
-    } catch (err) {
-      setError("Failed to delete post.");
-    } finally {
-      setLoading(false);
-    }
-  };
+  const {
+    createPost,
+    deletePost,
+    fetchPosts,
+    updatePost,
+    loading,
+    error,
+    posts,
+    setNewPostName,
+    newPostName,
+  } = usePosts();
 
   useEffect(() => {
     const fetchData = async () => {
