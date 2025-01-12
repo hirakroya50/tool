33064c9b0b0fb3e2bc8c5945324e446f8ad2e33e
hirakroya50 <hirakroya50@gmail.com>
2025-01-12 19:17:49 +0530

    env name update

--- tool/src/lib/awsClient.ts
+++ tool/src/lib/awsClient.ts
@@ -3,8 +3,8 @@
 import { S3 } from "@aws-sdk/client-s3";
 if (
   !process.env.NEXT_PUBLIC_AWS_REGION ||
-  !process.env.NEXT_PUBLIC_ACCESS_KEY_ID ||
-  !process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY
+  !process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID ||
+  !process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY
 ) {
   throw new Error("AWS environment variables are not properly set");
 }
@@ -12,8 +12,8 @@ if (
 const s3 = new S3({
   region: process.env.NEXT_PUBLIC_AWS_REGION,
   credentials: {
-    accessKeyId: process.env.NEXT_PUBLIC_ACCESS_KEY_ID,
-    secretAccessKey: process.env.NEXT_PUBLIC_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
   },
 });
 
